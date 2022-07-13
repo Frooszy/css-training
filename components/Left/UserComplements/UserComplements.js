2ced66e722fb1b165bbd67cf131ca58ffdc49050
Joao Vitor <frooszy@Frooszy.localdomain>
2022-07-13 15:06:25 -0300

    Added new Component (UserComplements)

---
+++ components/Left/UserComplements/UserComplements.js
@@ -0,0 +1,55 @@
+import styles from './UserComplements.module.css'
+import { FiMapPin, FiTwitter } from 'react-icons/fi';
+import { BsFillBriefcaseFill, BsGithub, BsLinkedin } from 'react-icons/bs';
+import { TbWorld } from 'react-icons/tb';
+import { MdMailOutline } from 'react-icons/md';
+
+const Informations = [{ "Local": "Brasil", "Job": "Programador", "GH": "Frooszy", "Linkedin": "Frooszy", "Twitter": "Frooszy", "Site": "Frooszy.com", "Contato": "Frooszy@frooszy.com" }]
+
+
+function UserComplements() {
+
+    return (
+
+        <div className={styles.Complements}>
+            <div className={styles.Box}>
+                {Informations.map(info => (
+                    <div className={styles.Itens}>
+                        <div className={styles.ItemBox}>
+                            <FiMapPin />
+                            <p><strong>{info.Local}</strong></p>
+                        </div>
+                        <div className={styles.ItemBox}>
+                            <BsFillBriefcaseFill />
+                            <p><strong>{info.Job}</strong></p>
+                        </div>
+                        <div className={styles.ItemBox}>
+                            <BsGithub />
+                            <p><strong>{info.GH}</strong></p>
+                        </div>
+                        <div className={styles.ItemBox}>
+                            <BsLinkedin />
+                            <p><strong>{info.Linkedin}</strong></p>
+                        </div>
+                        <div className={styles.ItemBox}>
+                            <FiTwitter />
+                            <p><strong>{info.Twitter}</strong></p>
+                        </div>
+                        <div className={styles.ItemBox}>
+                            <TbWorld />
+                            <p><strong>{info.Site}</strong></p>
+                        </div>
+                        <div className={styles.ItemBox}>
+                            <MdMailOutline />
+                            <p><strong>{info.Contato}</strong></p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+
+    )
+
+}
+
+export default UserComplements
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,7 @@
 import UserInfo from './../components/Left/UserInfo/UserInfo.js'
 import UserData from '../components/Right/UserData/UserData.js'
 import UserProjects from '../components/Right/UserProjects/UserProjects.js'
+import UserComplements from '../components/Left/UserComplements/UserComplements.js'
 
 import styles from './../styles/dashboard.module.css'
 
@@ -9,6 +10,7 @@ export default function Dashboard() {
         <div className={styles.General}>
             <div className={styles.Left}>
                 <UserInfo />
+                <UserComplements />
             </div>
             <div className={styles.Right}>
                 <UserData />
