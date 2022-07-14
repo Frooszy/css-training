2fe36b180d9bbb4eb32abfce8bd8bdf85bb3683b
Joao Vitor <frooszy@Frooszy.localdomain>
2022-07-14 10:47:02 -0300

    Code Fixes

--- components/Left/UserComplements/UserComplements.js
+++ components/Left/UserComplements/UserComplements.js
@@ -4,8 +4,7 @@ import { BsFillBriefcaseFill, BsGithub, BsLinkedin } from 'react-icons/bs';
 import { TbWorld } from 'react-icons/tb';
 import { MdMailOutline } from 'react-icons/md';
 
-const Informations = [{ "Local": "Brasil", "Job": "Programador", "GH": "Frooszy", "Linkedin": "Frooszy", "Twitter": "Frooszy", "Site": "Frooszy.com", "Contato": "Frooszy@frooszy.com" }]
-
+const info = { "Local": "Brasil", "Job": "Programador", "GH": "Frooszy", "Linkedin": "Frooszy", "Twitter": "Frooszy", "Site": "Frooszy.com", "Contato": "Frooszy@frooszy.com" }
 
 function UserComplements() {
 
@@ -13,38 +12,36 @@ function UserComplements() {
 
         <div className={styles.Complements}>
             <div className={styles.Box}>
-                {Informations.map(info => (
-                    <div className={styles.Itens}>
-                        <div className={styles.ItemBox}>
-                            <FiMapPin />
-                            <p><strong>{info.Local}</strong></p>
-                        </div>
-                        <div className={styles.ItemBox}>
-                            <BsFillBriefcaseFill />
-                            <p><strong>{info.Job}</strong></p>
-                        </div>
-                        <div className={styles.ItemBox}>
-                            <BsGithub />
-                            <p><strong>{info.GH}</strong></p>
-                        </div>
-                        <div className={styles.ItemBox}>
-                            <BsLinkedin />
-                            <p><strong>{info.Linkedin}</strong></p>
-                        </div>
-                        <div className={styles.ItemBox}>
-                            <FiTwitter />
-                            <p><strong>{info.Twitter}</strong></p>
-                        </div>
-                        <div className={styles.ItemBox}>
-                            <TbWorld />
-                            <p><strong>{info.Site}</strong></p>
-                        </div>
-                        <div className={styles.ItemBox}>
-                            <MdMailOutline />
-                            <p><strong>{info.Contato}</strong></p>
-                        </div>
+                <div className={styles.Itens}>
+                    <div className={styles.ItemBox}>
+                        <FiMapPin />
+                        <p><strong>{info.Local}</strong></p>
                     </div>
-                ))}
+                    <div className={styles.ItemBox}>
+                        <BsFillBriefcaseFill />
+                        <p><strong>{info.Job}</strong></p>
+                    </div>
+                    <div className={styles.ItemBox}>
+                        <BsGithub />
+                        <p><strong>{info.GH}</strong></p>
+                    </div>
+                    <div className={styles.ItemBox}>
+                        <BsLinkedin />
+                        <p><strong>{info.Linkedin}</strong></p>
+                    </div>
+                    <div className={styles.ItemBox}>
+                        <FiTwitter />
+                        <p><strong>{info.Twitter}</strong></p>
+                    </div>
+                    <div className={styles.ItemBox}>
+                        <TbWorld />
+                        <p><strong>{info.Site}</strong></p>
+                    </div>
+                    <div className={styles.ItemBox}>
+                        <MdMailOutline />
+                        <p><strong>{info.Contato}</strong></p>
+                    </div>
+                </div>
             </div>
         </div>
 
